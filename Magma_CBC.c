b82eb05bb8a3b244b294e15be29326faba8fee5a
Dmitriy <ddzuev@gmail.com>
2021-11-27 00:12:21 -0800

    ...

--- Magma_CBC.c
+++ Magma_CBC.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 //PRF_GOSTR3411(u_int32_t SecurityParameters_enc_key_length, u_int32_t SecurityParameters_mac_key_length, u_int8_t SecurityParameters_fixed_iv_length);
 //key_block = PRF(SecurityParameters.master_secret,"key expansion",SecurityParameters.server_random + SecurityParameters.client_random);
 
--- Magma_CBC_main.c
+++ Magma_CBC_main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-#include <Magma_CBC.c>
+#include <inttypes.h>
+uint32_t key[32];
+uint32_t L[32];
+uint32_t R[32];
 
-int main(){
-	printf(T(0x12);
-}
+uint32_t encryption(uint32_t key[32],uint32_t L[32],uint32_t R[32]);
+	int main(){
+uint32_t key[32] = {0x12,0x04,0x06,0x02,0x10,0x05,0x11,0x09,0x014,0x08,0x13,0x07,0x00,0x03,0x15,0x01};
+	uint32_t L[32];
+uint32_t R[32];
+	printf(encryption(key[32],L[32],R[32]), "%llu");
+
 
 return 0;
-
+}
